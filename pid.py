e0b06bea45d37f9405c898c24b25e9423b73e525
Vlad Ivanov <vladivanov.dev@gmail.com>
2019-02-06 14:23:04 +0300

    new class PID

--- pid.py
+++ pid.py
@@ -1,42 +1,26 @@
 from time import time
-''' pid obj prototype
-pid_obj = {
-    'setup': (0, 0, 0, 0), #kp, ki, kd, initial_value
 
-    'error_i': 0,
-    'ini_val': 0,
-    'last_time': 0,
+class PID:
+    def __init__(self, setup, motor):
+        self.motor = motor
+        (kp, ki, kd) = setup
+        self.state = dict.fromkeys(['last', 'now', 'wanted'])
+        (self.error_i, self.last_time) = (0, 0)
 
-    'state': dict.fromkeys(['last', 'now', 'wanted']),
+    def proc(self):
+        dt = (time.time() - self.last_time) / 1000
+        self.last_time = time.time()
+        error = self.state['wanted'] - self.state['now']
+        error_d = (error - (self.state['wanted'] - self.state['last'])) / dt
+        self.error_i += error * dt
 
-    Motor: <...>,
-    Sensor: <...>
-}
-'''
+        new_state = self.motor.position
 
-def reset_pid(pid_obj):
-    pid_obj['error_i'] = 0
-    pid_obj['ini_val'] = 0
-    pid_obj['last_time'] = 0
+        (self.state['last'], self.state['now']) = (self.state['now'], new_state)
+        Ut = kp * error + ki * self.error_i + kd * error_d
+        value = Ut
 
-def pid(pid_obj):
+        self.motor.run_direct(duty_cycle_sp=value)
 
-    dt = (time.time() - pid_obj['last_time']) / 1000
-    pid_obj['last_time'] = time.time()
-
-    (kp, ki, kd, ini_val) = pid_obj['setup']
-
-    error = state['wanted'] - state['now']
-    error_d = (error - (state['wanted'] - state['last'])) / dt
-    pid_obj['error_i'] += error * dt
-
-    new_state = 0
-    #new_state = Sensor.<get_value>()
-
-    (pid_obj['state']['last'], pid_obj['state']['now']) = (pid_obj['state']['now'], new_state)
-
-    Ut = kp * error + ki * pid_obj['error_i'] + kd * error_d
-
-    value = ini_val + Ut
-
-    #Motor.<set_value>()
+    def reset(self):
+        (self.last_time, self.error_i) = (0, 0)
